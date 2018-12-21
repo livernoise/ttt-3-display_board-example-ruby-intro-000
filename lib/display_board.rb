43ae59624f8a00f4e9996770ae3d77530f8dffd2
Ali LaVanway <alilavanway@gmail.com>
2018-12-21 23:02:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,8 @@
 def display_board
-    puts "A Tic Tac Toe Board"
-  end
-
-display_board
-
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end  
 
